7e2de3e80eac38332b3ec4281cb192d9736bcbd8
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-08-24 18:14:56 +0200

    run black -l 99

--- Conway's Game of Life/Code/visualizer.py
+++ Conway's Game of Life/Code/visualizer.py
@@ -5,7 +5,7 @@ from IPython import display
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-mpl.rcParams["figure.dpi"] = 200 # for plots with higher resolution
+mpl.rcParams["figure.dpi"] = 200  # for plots with higher resolution
 
 
 def plot_grid(grid):
@@ -25,17 +25,19 @@ def plot_grid(grid):
 
     # make a figure + axes
     fig, ax = plt.subplots(1, 1, tight_layout=True)
-    ax.axis('off')
+    ax.axis("off")
     num_rows, num_cols = grid.shape
-    
+
     # draw the grid
     for x in range(num_rows + 1):
-        ax.axhline(x, lw=0.1, color='k', zorder=2)
+        ax.axhline(x, lw=0.1, color="k", zorder=2)
     for y in range(num_cols + 1):
-        ax.axvline(y, lw=0.1, color='k', zorder=2)
-    
+        ax.axvline(y, lw=0.1, color="k", zorder=2)
+
     # draw the cells
-    ax.imshow(grid, interpolation='none', cmap ="binary", extent=[0, num_rows, 0, num_cols], zorder=0)
+    ax.imshow(
+        grid, interpolation="none", cmap="binary", extent=[0, num_rows, 0, num_cols], zorder=0
+    )
     plt.show()
 
 
